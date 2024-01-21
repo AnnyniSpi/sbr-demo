4b1fe3161ecaba31a53274e2791e2ad5174cd51c
Anna <annaspi333@gmail.com>
2024-01-21 12:43:28 +0200

    init project

---
+++ src/main/java/dev/annynispi/sbrdemo/entity/Student.java
@@ -0,0 +1,10 @@
+package dev.annynispi.sbrdemo.entity;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+}
